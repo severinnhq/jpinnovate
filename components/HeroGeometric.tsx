07e965f1bfe354eeb235a86aa7038c5244a431de
severinnhq <sstierbusiness@gmail.com>
2025-02-26 16:09:23 +0100

    ready

--- components/HeroGeometric.tsx
+++ components/HeroGeometric.tsx
@@ -7,25 +7,25 @@ import { cn } from "@/lib/utils";
 
 function ElegantShape({
     className,
-    delay = 0,
     width = 400,
     height = 100,
     rotate = 0,
     gradient = "from-white/[0.08]",
+    index = 0,
 }: {
     className?: string;
-    delay?: number;
     width?: number;
     height?: number;
     rotate?: number;
     gradient?: string;
+    index?: number;
 }) {
     return (
         <motion.div
             initial={{
-                opacity: 0.5,
-                y: -20, // Minimal distance
-                rotate: rotate - 2, // Minimal rotation
+                opacity: 0,
+                y: -150,
+                rotate: rotate - 15,
             }}
             animate={{
                 opacity: 1,
@@ -33,10 +33,10 @@ function ElegantShape({
                 rotate: rotate,
             }}
             transition={{
-                duration: 0.3, // Very quick animation
-                delay: delay * 0.1, // Minimal delay
+                duration: 2.4,
+                delay: 0, // No delay
                 ease: [0.23, 0.86, 0.39, 0.96],
-                opacity: { duration: 0.2 }, // Very quick fade
+                opacity: { duration: 1.2 },
             }}
             className={cn("absolute", className)}
         >
@@ -81,13 +81,13 @@ function HeroGeometric({
     title2?: string;
 }) {
     const fadeUpVariants = {
-        hidden: { opacity: 0.8, y: 5 }, // Minimal movement
+        hidden: { opacity: 0, y: 30 },
         visible: (i: number) => ({
             opacity: 1,
             y: 0,
             transition: {
-                duration: 0.2, // Very quick
-                delay: 0.05 + i * 0.05, // Minimal staggered delay
+                duration: 1,
+                delay: 0, // No initial delay
                 ease: [0.25, 0.4, 0.25, 1],
             },
         }),
@@ -99,48 +99,48 @@ function HeroGeometric({
 
             <div className="absolute inset-0 overflow-hidden">
                 <ElegantShape
-                    delay={0.05}
                     width={600}
                     height={140}
                     rotate={12}
                     gradient="from-indigo-500/[0.15]"
                     className="left-[-10%] md:left-[-5%] top-[15%] md:top-[20%]"
+                    index={0}
                 />
 
                 <ElegantShape
-                    delay={0.1}
                     width={500}
                     height={120}
                     rotate={-15}
                     gradient="from-rose-500/[0.15]"
                     className="right-[-5%] md:right-[0%] top-[70%] md:top-[75%]"
+                    index={1}
                 />
 
                 <ElegantShape
-                    delay={0.07}
                     width={300}
                     height={80}
                     rotate={-8}
                     gradient="from-violet-500/[0.15]"
                     className="left-[5%] md:left-[10%] bottom-[5%] md:bottom-[10%]"
+                    index={2}
                 />
 
                 <ElegantShape
-                    delay={0.12}
                     width={200}
                     height={60}
                     rotate={20}
                     gradient="from-amber-500/[0.15]"
                     className="right-[15%] md:right-[20%] top-[10%] md:top-[15%]"
+                    index={3}
                 />
 
                 <ElegantShape
-                    delay={0.15}
                     width={150}
                     height={40}
                     rotate={-25}
                     gradient="from-cyan-500/[0.15]"
                     className="left-[20%] md:left-[25%] top-[5%] md:top-[10%]"
+                    index={4}
                 />
             </div>
 
